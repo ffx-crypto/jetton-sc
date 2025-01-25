3ade4a5ca8e5089428219d56d96c854a64b379fb
gilert <crankyfox@protonmail.com>
2025-01-25 12:29:31 +0100

    add reciever address in mintJetton function

--- scripts/mintJettons.ts
+++ scripts/mintJettons.ts
@@ -7,17 +7,21 @@ export async function run(provider: NetworkProvider) {
     const ui = provider.ui();
 
     const minterAddress = await ui.input('Provide Minter address');
+    const recieverAddress = await ui.input("Provide reciever address");
     const jettonAmountStr = await ui.input('Provide amount of jettons to mint');
     const jettonAmount = Number(jettonAmountStr);
 
     if (isNaN(jettonAmount)) {
         throw new Error('Invalid input: entered value is not a number');
     }
+    if (!Address.isAddress(address(recieverAddress))) {
+        return;
+    }
     
     const jettonMinter = provider.open(JettonMinter.createFromAddress(address(minterAddress)));
     // mint fees = 0.006957432
     await jettonMinter.sendMint(provider.sender(), 
-        provider.sender().address as Address, // to address
+        address(recieverAddress), // to address
         toNano(jettonAmount), 
         1n, // forward ton amount
         toNano('0.05') // total ton amount
